85c36d539a8bc519b0c299dcdead2bc8af40d3a8
Poul Hornsleth <phornsleth@synapsloans.com>
2018-08-07 15:23:10 -0400

    uncrustify

--- single.php
+++ single.php
@@ -5,12 +5,12 @@
  * @package parallax-one
  */
 
-	get_header();
+    get_header();
 ?>
 
-	</div>
-	<!-- /END COLOR OVER IMAGE -->
-	<?php parallax_hook_header_bottom(); ?>
+</div>
+<!-- /END COLOR OVER IMAGE -->
+<?php parallax_hook_header_bottom(); ?>
 </header>
 <!-- /END HOME / HEADER  -->
 <?php parallax_hook_header_after(); ?>
@@ -21,39 +21,43 @@
 
 		<div id="primary" class="content-area 
 		<?php
-		if ( is_active_sidebar( 'sidebar-1' ) ) {
-			echo 'col-md-8';
-		} else {
-			echo 'col-md-12';}
+        if (is_active_sidebar('sidebar-1')) {
+            echo 'col-md-8';
+        } else {
+            echo 'col-md-12';
+        }
 ?>
 ">
 			<main itemscope itemtype="http://schema.org/WebPageElement" itemprop="mainContentOfPage" id="main" class="site-main" role="main">
-<!-- A2: Single Post single.php  Container that calls content-single.php -->
-			<?php
-			while ( have_posts() ) :
-				the_post();
+				<!-- A2: Single Post single.php  Container that calls content-single.php -->
+				<?php
+            while (have_posts()) :
+                the_post();
 ?>
 
-				<?php get_template_part( 'content', 'single' ); ?>
+				<?php get_template_part('content', 'single'); ?>
 
 				<?php the_post_navigation(); ?>
 
 				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-					endif;
-				?>
+                    // If comments are open or we have at least one comment, load up the comment template
+                if (comments_open() || get_comments_number()) :
+                    comments_template();
+                    endif;
+                ?>
 
-			<?php endwhile; ?>
+				<?php endwhile; ?>
 
-			</main><!-- #main -->
-		</div><!-- #primary -->
+			</main>
+			<!-- #main -->
+		</div>
+		<!-- #primary -->
 
 		<?php get_sidebar(); ?>
 
 	</div>
 	<?php parallax_hook_single_bottom(); ?>
-</div><!-- .content-wrap -->
+</div>
+<!-- .content-wrap -->
 <?php parallax_hook_single_after(); ?>
-<?php get_footer(); ?>
+<?php get_footer();
