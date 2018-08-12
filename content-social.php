b97c7df6ef311ec460578c4acc4e0116e312a95b
Poul Hornsleth <poulh@umich.edu>
2018-08-12 07:30:12 -0400

    social url generation much improved

--- content-social.php
+++ content-social.php
@@ -1,23 +1,19 @@
-<?php
-$social_fields = array("amazon_authors_page", "twitter", "facebook", "website","email");
-$social_tooltips = array("%s Amazon Homepage: Proceeds benefit club!", "%s on Twitter", "%s on Facebook", "%s's Website", "Email %s");
-$social_icons = array("fa fa-amazon", "fa fa-twitter", "fa fa-facebook", "fa fa-globe", "fa fa-envelope");
+<?php //$bioName, $amazonAffiliateTag must be set before this template is loaded
+$socialFields = array("amazon_authors_page", "twitter", "facebook", "website","email");
+$socialUrls = array("%s%s", "https://www.twitter.com/%s", "https://www.facebook.com/%s", "%s","mailto:%s");
+$socialTooltips = array("%s Amazon Homepage: Proceeds benefit club!", "%s on Twitter", "%s on Facebook", "%s's Website", "Email %s");
+$socialIcons = array("fa fa-amazon", "fa fa-twitter", "fa fa-facebook", "fa fa-globe", "fa fa-envelope");
 $amazonAffiliateTag = 'westsidegop20-20';
-$affiliateUrlParam = '?tag=' . $amazonAffiliateTag;
-foreach ($social_fields as $idx=>$social_field) {
-    $social_tooltip = sprintf($social_tooltips[$idx], $bioName, $affiliateUrlParam);
-    $social_url = get_field($social_field, $bioID);
-    
-    if ($social_url) {
-        if ($social_field == "email") {
-            //todo: this is a bit hacky
-            $social_url = 'mailto:' . $social_url;
-        } elseif ($social_field == "amazon_authors_page") {
-            //todo also hacky
-            $social_url = $social_url . $affiliateUrlParam;
-        } ?>
-        <a target="0" title="<?php echo $social_tooltip; ?>" href="<?php echo $social_url; ?>">
-        <i class="<?php echo $social_icons[$idx] ?> fa-lg"></i>
+$affiliateUrlParam = $amazonAffiliateTag ? '?tag=' . $amazonAffiliateTag : "";
+foreach ($socialFields as $idx=>$socialField) {
+
+    $socialVal = get_field($socialField, $bioID);
+    if ($socialVal) {
+        $socialTooltip = sprintf($socialTooltips[$idx], $bioName, $affiliateUrlParam);
+        $socialUrl = sprintf($socialUrls[$idx],$socialVal,$affiliateUrlParam);
+ ?>
+        <a target="0" title="<?php echo $socialTooltip; ?>" href="<?php echo $socialUrl; ?>">
+        <i class="<?php echo $socialIcons[$idx] ?> fa-lg"></i>
         </a>
 <?php } ?>
 <?php } ?>
